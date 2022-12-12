2b0e11f9c6f40c1a0fd7d47f310329f4b15a88d5
arrase <arrase21@gmail.com>
2022-12-12 12:54:23 -0500

    remove fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Sidebar from './scenes/global/Sidebar'
@@ -16,46 +16,37 @@ import { ColorModeContext, useMode } from './theme'
 import Calendar from './scenes/calendar/calendar'
 
 function App() {
-  const [theme, colorMode] = useMode()
-  const [isSidebar, setIsSidebar] = useState(true)
 
-  fetch('https://macro-api.onrender.com', {
-    credentials: 'omit',
-  })
+    fetch("https://macro-api.onrender.com")
+        .then(res => res.text())
+        .then(res => this.setState({ apiResponse: res }));
 
-  return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <div className='app'>
-          <header className='App-header'></header>
-          <BrowserRouter>
-            <Sidebar isSidebar={isSidebar} />
-            <main className='content'>
-              <Topbar setIsSidebar={setIsSidebar} />
-              <Routes>
-                <Route path='/clientes/' element={<CompMostrarCliente />} />
-                <Route
-                  path='/clientes/agregar'
-                  element={<CompAgregarClientes />}
-                />
-                <Route
-                  path='/clientes/editar/:id'
-                  element={<CompEditarClientes />}
-                />
-                <Route path='/comidas/' element={<CompMostrarComidas />} />
-                <Route
-                  path='/comidas/editar/:id'
-                  element={<CompEditarComidas />}
-                />
-                <Route path='/calendar/' element={<Calendar />} />
-                <Route path='/contacto/' element={<ContactUs />} />
-              </Routes>
-            </main>
-          </BrowserRouter>
-        </div>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
-  )
+	const [theme, colorMode] = useMode()
+	const [isSidebar, setIsSidebar] = useState(true)
+	return (
+		<ColorModeContext.Provider value={colorMode}>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<div className='app'>
+					<header className='App-header'></header>
+					<BrowserRouter>
+						<Sidebar isSidebar={isSidebar} />
+						<main className='content'>
+							<Topbar setIsSidebar={setIsSidebar} />
+							<Routes>
+								<Route path='/clientes/' element={<CompMostrarCliente />} />
+								<Route path='/clientes/agregar' element={<CompAgregarClientes />}/>
+								<Route path='/clientes/editar/:id' element={<CompEditarClientes />}/>
+								<Route path='/comidas/' element={<CompMostrarComidas />} />
+                <Route path='/comidas/editar/:id' element={<CompEditarComidas />}/>
+								<Route path='/calendar/' element={<Calendar />} />
+								<Route path='/contacto/' element={<ContactUs />} />
+							</Routes>
+						</main>
+					</BrowserRouter>
+				</div>
+			</ThemeProvider>
+		</ColorModeContext.Provider>
+	)
 }
 export default App
